17490e6d4df8419b669507385ee96d4b688f8b62
SimpleSols <danwilliams23.dw@gmail.com>
2016-06-23 21:30:26 +0100

    Initial commit

---
+++ app/src/main/java/uk/me/danwilliams/drinkscorecard/MainActivity.java
@@ -0,0 +1,25 @@
+package uk.me.danwilliams.drinkscorecard;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ListActivity {
+    private Hole[] mHoles = new Hole[18];
+    private ListAdapter mListAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialise holes
+        int strokes = 0;
+        for (int i = 0; i < mHoles.length; i++) {
+            mHoles[i] = new Hole("Hole " + (i + 1) + " :", strokes);
+        }
+
+        mListAdapter = new ListAdapter(this, mHoles);
+        setListAdapter(mListAdapter);
+    }
+}
